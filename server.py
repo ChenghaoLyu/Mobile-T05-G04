556d682613a4c807acb31c0ca0d242f0299b816c
jinjied <jinjied@student.unimelb.edu.au>
2023-09-30 23:05:08 +1000

    add basic server framework and client connection

---
+++ server.py
@@ -0,0 +1,11 @@
+# server.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return jsonify(message="Hello, Android Client!")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
