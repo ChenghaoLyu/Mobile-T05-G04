46a6591fb3de67ed1704bd0add35de537d6842ce
lyu <chlyu@student.unimelb.edu.au>
2023-10-05 16:57:32 +1100

    initial push

---
+++ G04Project/app/src/main/java/com/example/g04_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.g04_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
