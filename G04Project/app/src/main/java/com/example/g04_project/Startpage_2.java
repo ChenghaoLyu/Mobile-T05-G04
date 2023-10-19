5c37a8b5a22918a28e8264470603ccde2039f032
Xiaochen <xhhou@student.unimelb.edu.au>
2023-10-19 17:00:13 +1100

    Editing the interfaces styles for start pages, login&register, homepage, and new game page

--- G04Project/app/src/main/java/com/example/g04_project/Startpage_2.java
+++ G04Project/app/src/main/java/com/example/g04_project/Startpage_2.java
@@ -2,13 +2,27 @@ package com.example.g04_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Startpage_2 extends AppCompatActivity {
+
+    private Button goto_home_page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startpage2);
+        goto_home_page = findViewById(R.id.goto_home_page);
+
+        goto_home_page.setOnClickListener(view ->{
+            open_home_page(view);
+        });
+    }
+
+    public void open_home_page(View view){
+        startActivity(new Intent(this, HomePage.class));
     }
 }
